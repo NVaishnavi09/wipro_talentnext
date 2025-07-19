691276369061a8298175bdf8928d86b229271556
Vaishnavi <vaishnavi.natla@gmail.com>
2025-07-19 21:35:07 +0530

    files added

---
+++ collection/src/collection/Question20.java
@@ -0,0 +1,33 @@
+package collection;
+
+import java.util.*;
+import java.io.*;
+import java.util.function.Supplier;
+
+public class Question20 {
+	  public static void main(String[] args) {
+	        Supplier<List<Integer>> primeSupplier = () -> {
+	            List<Integer> primes = new ArrayList<>();
+	            int num = 2;
+	            while (primes.size() < 10) {
+	                if (isPrime(num)) {
+	                    primes.add(num);
+	                }
+	                num++;
+	            }
+	            return primes;
+	        };
+
+	        List<Integer> primeList = primeSupplier.get();
+	        System.out.println("First 10 Prime Numbers: " + primeList);
+	    }
+
+	    static boolean isPrime(int n) {
+	        if (n < 2) return false;
+	        for (int i = 2; i <= Math.sqrt(n); i++) {
+	            if (n % i == 0) return false;
+	        }
+	        return true;
+	    }
+	}
+
